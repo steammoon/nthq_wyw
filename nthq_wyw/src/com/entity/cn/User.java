cc244e495f40169ea64b25e14fcb026d616724df
steammoon <39254660+steammoon@users.noreply.github.com>
2019-04-08 13:24:48 +0800

    新增user模型

---
+++ nthq_wyw/src/com/entity/cn/User.java
@@ -0,0 +1,67 @@
+package com.entity.cn;
+
+public class User {
+	private String id;//ID
+	private String username;//用户名
+	private String password;//密码
+	private String phoneNum;//手机号
+	private String email;//邮箱
+	private String createtime;//创建日期
+	private String effective;//有效性
+	private String cardnum;//身份证号
+	private String userdetail;//身份证号
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCreatetime() {
+		return createtime;
+	}
+	public void setCreatetime(String createtime) {
+		this.createtime = createtime;
+	}
+	public String getEffective() {
+		return effective;
+	}
+	public void setEffective(String effective) {
+		this.effective = effective;
+	}
+	public String getCardnum() {
+		return cardnum;
+	}
+	public void setCardnum(String cardnum) {
+		this.cardnum = cardnum;
+	}
+	public String getUserdetail() {
+		return userdetail;
+	}
+	public void setUserdetail(String userdetail) {
+		this.userdetail = userdetail;
+	}
+}
