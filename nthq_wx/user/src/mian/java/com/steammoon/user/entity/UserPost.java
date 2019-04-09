eb6f411b5e5283024f9c9d013665de4620d1ea18
steammoon <39254660+steammoon@users.noreply.github.com>
2019-04-09 15:18:27 +0800

    index 123

---
+++ nthq_wx/user/src/mian/java/com/steammoon/user/entity/UserPost.java
@@ -0,0 +1,53 @@
+package com.steammoon.user.entity;
+
+public class UserPost {
+	private String id;//ID
+	private String userid;//用户ID
+	private String depid;//部门ID
+	private String orgid;//组织ID
+	private String postid;//岗位ID
+	private String createtime;//创建日期
+	private String effective;//有效性
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getDepid() {
+		return depid;
+	}
+	public void setDepid(String depid) {
+		this.depid = depid;
+	}
+	public String getOrgid() {
+		return orgid;
+	}
+	public void setOrgid(String orgid) {
+		this.orgid = orgid;
+	}
+	public String getPostid() {
+		return postid;
+	}
+	public void setPostid(String postid) {
+		this.postid = postid;
+	}
+	public String getCreatetime() {
+		return createtime;
+	}
+	public void setCreatetime(String createtime) {
+		this.createtime = createtime;
+	}
+	public String getEffective() {
+		return effective;
+	}
+	public void setEffective(String effective) {
+		this.effective = effective;
+	}
+}
